f0ef5655882a64f39b235acca2d27baeb287492d
Jason Sahl <jsahl@tgen.org>
2013-04-10 15:06:54 -0700

    updated gene caller

--- wg_bsr_v5.py
+++ wg_bsr_v5.py
@@ -2,7 +2,9 @@
 
 """Calculate the BSR value for all predicted ORFs
 in a set of genomes in fasta format.  V3 - replaced
-transeq with BioPython"""
+transeq with BioPython.  V4 - changed to true BSR
+test.  V5 - fixed bug in how BSR was calculated.
+V6 - changed gene caller from Glimmer to Prodigal"""
 
 import Bio
 from Bio import SeqIO
@@ -23,30 +25,8 @@ def get_seq_name(in_fasta):
     """used for renaming the sequences"""
     return os.path.basename(in_fasta)
 
-def join_contigs(directory):
-    """iterate over each genome in a directory;
-    for draft genomes, concatenate all contigs into
-    a single fasta file, with contigs separated by a linker
-    that includes a stop codon in all frames"""
-    fout = open("out.txt", "w")
-    for infile in glob.glob(os.path.join(directory, '*.fasta')):
-	names = get_seq_name(infile)
-	reduced = os.path.splitext(names)[0]
-	fout.write('>' + str(reduced) + '\n')
-        for record in SeqIO.parse(open(infile), "fasta"):
-            fout.write(str(record.seq) + "NNNNNCACACACTTAATTAATTAAGTGTGTGNNNNN" + '\n')
-        fout.write('\n')
-    fout.close()
-        
-def split_fasta():
-    """split sequences in fasta and fix formatting"""
-    infile = open("out.txt", "rU")
-    for record in SeqIO.parse(infile, "fasta"):
-            f_out = os.path.join(record.id + "_joined" + '.fasta')
-            SeqIO.write([record], open(f_out, "w"), "fasta")
-  
 def predict_genes(directory, processors):
-    """simple gene prediction using glimmer in order
+    """simple gene prediction using Prodigal in order
     to find coding regions from a genome sequence"""    
     os.chdir("%s/joined" % directory)
     curr_dir=os.getcwd()
@@ -57,80 +37,31 @@ def predict_genes(directory, processors):
     def _perform_workflow(data):
         tn, f = data
         names = get_seq_name(f)
-        if "_joined.fasta" in names:
-		reduced = names.replace('_joined.fasta','')
-		
-        print reduced
-        subprocess.check_call("g3-iterated.csh %s %s > /dev/null 2>&1" % (f, reduced), shell=True)
-        outseqrecords = []
-        glimmer_file = "%s.predict" % reduced
-        raw_file = open("%s_joined.fasta" % reduced, "rU")
-        seq_record = SeqIO.parse(raw_file, "fasta").next()
-        for inline in file(glimmer_file):
-            if '>' in inline:
-                seqname = inline.split()[0][1:]
-                continue
-            if "orf" not in inline:
-                continue
-            orfname, sbegin, send, rf, score = inline.strip().split()
-            sbegin = int(sbegin)
-            send = int(send)
-            rf = int(rf)
-            """ process the reverse complement"""
-            if rf < 0:
-                sbegin, send = send, sbegin
-            sbegin -= 1
-            score = float(score)
-            """split the sequence record"""
-            newseq = seq_record.seq[sbegin:send]
-            if rf < 0:
-                newseq = newseq.reverse_complement()
-            seqrecord_description = "begin=%d end=%d rf=%d score=%.2f" % (sbegin+1, send, rf, score)
-            outseqrecords.append(SeqRecord(newseq,id=seqname+"_"+orfname, description=seqrecord_description))
-        outhandle = open("%s.genes" % reduced, "w")
-        SeqIO.write(outseqrecords,outhandle,"fasta")
-        outhandle.close()
-
+        reduced = names.replace('.fasta.new','')
+        subprocess.check_call("prodigal -i %s -d %s_genes.seqs -a %s_genes.pep > /dev/null 2>&1" % (f, reduced, reduced), shell=True)
+        
     results = set(p_func.pmap(_perform_workflow,
                               files_and_temp_names,
                               num_workers=processors))
 
-def sort_seqs():
-    """filter out short sequences from a multifasta"""
-    curr_dir=os.getcwd()
-    for infile in glob.glob(os.path.join(curr_dir, "*genes")):
-        long_sequences = [ ]
-        outfile = open("%s.long" % infile, "w")
-        for record in SeqIO.parse(infile, "fasta"):
-            if len(record.seq) > int(100):
-                long_sequences.append(record)
-        SeqIO.write(long_sequences, outfile, "fasta")
-        outfile.close()
-
 def uclust_sort():
-    """sort with Usearch.  Need to update to v6 once
-    bugs are fixed"""
-    cmd = ["usearch", 
-           "--sort", "all_long.txt",
-           "-output", "all_sorted.txt",
-           "--maxlen", "50000"]
+    """sort with Usearch. Updated to V6"""
+    cmd = ["usearch6", 
+           "-sortbylength", "all_gene_seqs.out",
+           "-output", "all_sorted.txt"]
     subprocess.check_call(cmd)
 
-def uclust_cluster():
-    """cluster with Uclust.  Need to update.  May produce
-    a huge number of clusters for some organisms"""
-    cmd = ["usearch",
-           "--cluster", "all_sorted.txt",
-           "--id", "0.90",
-           "--uc", "results.uc",
-           "--consout", "consensus.fasta",
-           "--iddef", "3",
-           "--maxlen", "50000"]
+def uclust_cluster(id):
+    """cluster with Uclust.  Updated to V6"""
+    cmd = ["usearch6",
+           "-cluster_fast", "all_sorted.txt",
+           "-id", str(id),
+           "-uc", "results.uc",
+           "-centroids", "consensus.fasta"]
     subprocess.check_call(cmd)
 
 def translate_consensus(consensus):
-    """translate nucleotide into peptide with transeq"""
-    #subprocess.check_call("transeq -sequence consensus.fasta -outseq consensus.pep -frame 1 -trim > /dev/null 2>&1", shell=True)
+    """translate nucleotide into peptide with BioPython"""
     infile = open(consensus, "rU")
     output_handle = open("tmp.pep", "w")
     for record in SeqIO.parse(infile, "fasta"):
@@ -138,7 +69,9 @@ def translate_consensus(consensus):
         print >> output_handle, record.seq.translate(to_stop=True)
 
 def filter_seqs(input_pep):
-    """filter out short sequences from a multifasta"""
+    """filter out short sequences from a multifasta.
+    Will hopefully speed up the process without losing
+    important information"""
     long_sequences = [ ]
     infile = open(input_pep, "rU")
     outfile = open("consensus.pep", "w")
@@ -148,22 +81,18 @@ def filter_seqs(input_pep):
     SeqIO.write(long_sequences, outfile, "fasta")
     outfile.close()
 
-def blast_against_each_genome(directory, processors):
+def blast_against_each_genome(directory, processors, filter):
     """BLAST all peptides against each genome"""
     curr_dir=os.getcwd()
     files = os.listdir(curr_dir)
     files_and_temp_names = [(str(idx), os.path.join(curr_dir, f))
                             for idx, f in enumerate(files)]
     def _perform_workflow(data):
-	"""I'd like to filter query, but this really makes
-	the script slow"""
-        tn, f = data
+	tn, f = data
         names = get_seq_name(f)
-        if "_joined.fasta" in names:
-	    reduced = names.replace('_joined.fasta','')
-        if "_joined.fasta" in f:
+        if ".fasta.new" in f:
             subprocess.check_call("formatdb -i %s -p F > /dev/null 2>&1" % f, shell=True)
-        if "_joined.fasta" in f:
+        if ".fasta.new" in f:
             cmd = ["blastall",
                    "-p", "tblastn",
                    "-i", "consensus.pep",
@@ -173,7 +102,7 @@ def blast_against_each_genome(directory, processors):
                    "-v", "1",
                    "-e", "0.1",
                    "-m", "8",
-                   #"-F", "F",
+                   "-F", str(filter),
                    "-o", "%s_blast.out" % f]
             subprocess.check_call(cmd)
             
@@ -229,7 +158,7 @@ def make_table(directory):
         name.append(out)
         reduced=[ ]
         """remove the junk at the end of the file"""
-	for x in name:reduced.append(x.replace('_joined.fasta_blast.out.filtered.filtered.unique',''))
+	for x in name:reduced.append(x.replace('.fasta.new_blast.out.filtered.filtered.unique',''))
         names.append(reduced)
         dict={}
         file=open(infile, "rU")
@@ -268,23 +197,27 @@ def divide_values(file, ref_scores):
         fields=line.split()
         all_fields=list(fields)
         fields=map(float, fields[1:])
-        #largest=max(fields)
         values= [ ]
+        """v7 - added in exception to address bug"""
 	for x in fields:
-	    values.append(float(x)/float(ref_scores.get(all_fields[0])))
+            try:
+                values.append(float(x)/float(ref_scores.get(all_fields[0])))
+            except:
+                values.append(float(x)/float("1000"))
         sort_values=['%.2f' % elem for elem in values]
         print >> outfile, '\t'.join([str(item) for item in sort_values])
 
-def blast_against_self(genes_nt, genes_pep, output):
+def blast_against_self(genes_nt, genes_pep, output, filter):
     cmd = ["blastall",
            "-p", "tblastn",
            "-i", genes_pep,
            "-d", genes_nt,
            "-a", "2",
-           "-b", "1",
-           "-v", "1",
+           #"-b", "1",
+           #"-v", "1",
            "-e", "0.1",
            "-m", "8",
+           "-F", str(filter),
            "-o", output]
     subprocess.check_call(cmd)
 
@@ -297,30 +230,30 @@ def parse_self_blast(blast_out):
         my_dict.update({str1:str2})
     return my_dict
 
-def main(directory, processors):
-    join_contigs(directory)
-    split_fasta()
+def main(directory, id, filter, processors):
     try:
      	os.makedirs('%s/joined' % directory)
     except OSError, e:
      	if e.errno != errno.EEXIST:
             raise
-    os.system("mv *_joined.fasta %s/joined" % directory)
-    logging.logPrint("predicting genes")
+    for infile in glob.glob(os.path.join(directory, '*.fasta')):
+        name=get_seq_name(infile)
+        os.system("cp %s %s/joined/%s.new" % (infile,directory,name))
+    logging.logPrint("predicting genes with Prodigal")
     predict_genes(directory, processors)
-    sort_seqs()
-    os.system("cat *.long > all_long.txt")
+    logging.logPrint("Prodigal done")
+    os.system("cat *genes.seqs > all_gene_seqs.out")
     uclust_sort()
-    uclust_cluster()
+    uclust_cluster(id)
     translate_consensus("consensus.fasta")
     filter_seqs("tmp.pep")
     subprocess.check_call("formatdb -i consensus.fasta -p F", shell=True)
-    blast_against_self("consensus.fasta", "consensus.pep", "tmp_blast.out")
+    blast_against_self("consensus.fasta", "consensus.pep", "tmp_blast.out", filter)
     subprocess.check_call("sort -u -k 1,1 tmp_blast.out > self_blast.out", shell=True)
     ref_scores=parse_self_blast("self_blast.out")
     subprocess.check_call("rm tmp_blast.out self_blast.out", shell=True)
     logging.logPrint("starting BLAST")
-    blast_against_each_genome(directory, processors)
+    blast_against_each_genome(directory, processors, filter)
     logging.logPrint("BLAST done")
     parse_blast_report(directory)
     get_unique_lines(directory)
@@ -329,7 +262,7 @@ def main(directory, processors):
     divide_values("bsr_matrix", ref_scores)
     subprocess.check_call("paste ref.list BSR_matrix_values.txt > ../bsr_matrix_values.txt", shell=True)
     subprocess.check_call("cp names.txt consensus.pep ..", shell=True)
-    logging.logPrint("cleaning up")
+    logging.logPrint("all Done")
     #ap=os.path.abspath("%s"+"/" % directory)
     #os.system("rm -rf %sjoined" % ap)
     
@@ -339,6 +272,12 @@ if __name__ == "__main__":
     parser.add_option("-d", "--directory", dest="directory",
                       help="/path/to/fasta_directory [REQUIRED]",
                       action="store", type="string")
+    parser.add_option("-i", "--identity", dest="id",
+                      help="clustering id for USEARCH (0.0-1.0)",
+                      default="0.9", type="float")
+    parser.add_option("-f", "--filter", dest="filter",
+                      help="to use blast filtering or not",
+                      default="F", type="string")
     parser.add_option("-p", "--parallel_workers", dest="processors",
                       help="How much work to do in parallel, defaults to 2, should number of CPUs your machine has",
                       action="store", type="int")
@@ -351,5 +290,5 @@ if __name__ == "__main__":
             parser.print_help()
             exit(-1)
 
-    main(options.directory, options.processors)
+    main(options.directory, options.id, options.filter, options.processors)
 
