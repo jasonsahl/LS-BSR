d6701122d862dfa03ec4c1bccc01e2be936e12cb
Jason Sahl <jasonsahl@gmail.com>
2018-08-16 14:14:49 -0700

    updated script for multiple entries

--- tools/extract_locus_tags.py
+++ tools/extract_locus_tags.py
@@ -4,35 +4,34 @@
 extract the locus tags into
 a new file"""
 
-from __future__ import print_function
 from sys import argv
 from Bio import SeqIO
 import sys
 import os
 
 try:
-    infile = open(argv[1], "U")
+    open(argv[1], "U")
 except:
-    print("usage: script genbank_file.gbk")
+    print "usage: script genbank_file.gbk"
     sys.exit()
 
-curr_dir = os.getcwd()
-curr_path=os.path.abspath("%s" % curr_dir)
-record = SeqIO.read(infile, "genbank")
+#record = SeqIO.read(infile, "genbank")
 name = argv[1].split(".gbk")
-output_handle = open("%s/%s.fasta" % (curr_path,name[0]), "w")
+output_handle = open("%s.fasta" % name[0], "w")
 count = 0
-for feature in record.features:
-    if feature.type == "gene":
-        count = count + 1
-        try:
-            feature_name = feature.qualifiers["locus_tag"]
-            feature_seq = feature.extract(record.seq)
-            print(str(record.id)+"\t"+"".join(feature_name)+"\t"+str(feature.location))
-            # Simple FASTA output without line wrapping:
-            output_handle.write(">"+"".join(feature_name)+"\n"+str(feature_seq)+"\n")
-        except:
-            print("problem encountered extracting: %s, skipping.." % "".join(feature_name))
+with open(argv[1]) as infile:
+    for record in SeqIO.parse(infile, "genbank"):
+        for feature in record.features:
+            if feature.type == "CDS":
+                #print(feature)
+                count = count + 1
+                try:
+                    feature_name = feature.qualifiers["locus_tag"]
+                    feature_seq = feature.extract(record.seq)
+                    feature_product = feature.qualifiers["product"]
+                    # Simple FASTA output without line wrapping:
+                    output_handle.write(">" + "".join(feature_name) + "|" + "".join(feature_product) + "\n" + str(feature_seq) + "\n")
+                except:
+                    pass
 output_handle.close()
-os.system("mv %s.fasta %s" % (name[0],curr_path))
 print(str(count) + " CDS sequences extracted")
