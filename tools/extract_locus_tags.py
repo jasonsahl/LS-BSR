bfba70912c0ede47e917e7cbb0d83c8803af110d
Jason Sahl <jasonsahl@gmail.com>
2018-08-23 09:36:04 -0700

    reporting more informative error

--- tools/extract_locus_tags.py
+++ tools/extract_locus_tags.py
@@ -15,7 +15,6 @@ except:
     print "usage: script genbank_file.gbk"
     sys.exit()
 
-#record = SeqIO.read(infile, "genbank")
 name = argv[1].split(".gbk")
 output_handle = open("%s.fasta" % name[0], "w")
 count = 0
@@ -23,15 +22,17 @@ with open(argv[1]) as infile:
     for record in SeqIO.parse(infile, "genbank"):
         for feature in record.features:
             if feature.type == "CDS":
-                #print(feature)
-                count = count + 1
                 try:
                     feature_name = feature.qualifiers["locus_tag"]
                     feature_seq = feature.extract(record.seq)
                     feature_product = feature.qualifiers["product"]
+                    count = count + 1
                     # Simple FASTA output without line wrapping:
                     output_handle.write(">" + "".join(feature_name) + "|" + "".join(feature_product) + "\n" + str(feature_seq) + "\n")
                 except:
                     pass
 output_handle.close()
-print(str(count) + " CDS sequences extracted")
+if count == 0:
+    print("Problem with GenBank file. Make sure that it contains locus_tags")
+else:
+    print(str(count) + " CDS sequences extracted")
