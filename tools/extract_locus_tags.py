41be78f8c394d910d9ba86293442aa7428e3fced
Jason Sahl <jasonsahl@gmail.com>
2019-01-30 08:52:21 -0700

    updated script

--- tools/extract_locus_tags.py
+++ tools/extract_locus_tags.py
@@ -10,28 +10,26 @@ import sys
 import os
 
 try:
-    open(argv[1], "U")
+    name = argv[1].split(".gbk")
+    output_handle = open("%s.fasta" % name[0], "w")
+    count = 0
+    with open(argv[1]) as infile:
+        for record in SeqIO.parse(infile, "genbank"):
+            for feature in record.features:
+                if feature.type == "CDS":
+                    try:
+                        feature_name = feature.qualifiers["locus_tag"]
+                        feature_seq = feature.extract(record.seq)
+                        feature_product = feature.qualifiers["product"]
+                        count = count + 1
+                        # Simple FASTA output without line wrapping:
+                        output_handle.write(">" + "".join(feature_name) + "|" + "".join(feature_product) + "\n" + str(feature_seq) + "\n")
+                    except:
+                        pass
+    output_handle.close()
 except:
-    print "usage: script genbank_file.gbk"
+    print("usage: script genbank_file.gbk")
     sys.exit()
-
-name = argv[1].split(".gbk")
-output_handle = open("%s.fasta" % name[0], "w")
-count = 0
-with open(argv[1]) as infile:
-    for record in SeqIO.parse(infile, "genbank"):
-        for feature in record.features:
-            if feature.type == "CDS":
-                try:
-                    feature_name = feature.qualifiers["locus_tag"]
-                    feature_seq = feature.extract(record.seq)
-                    feature_product = feature.qualifiers["product"]
-                    count = count + 1
-                    # Simple FASTA output without line wrapping:
-                    output_handle.write(">" + "".join(feature_name) + "|" + "".join(feature_product) + "\n" + str(feature_seq) + "\n")
-                except:
-                    pass
-output_handle.close()
 if count == 0:
     print("Problem with GenBank file. Make sure that it contains locus_tags")
 else:
